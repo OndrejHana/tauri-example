4cca0b872d4ee42c389c2210d937709aa6fba43f
OndrejHana <ondrahana001@gmail.com>
2024-04-21 13:40:46 +0200

    window creation

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,24 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{AppHandle, WindowBuilder, WindowUrl};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn spawn_window(app_handle: AppHandle) {
+    println!("Spawning window");
+    let handle = WindowBuilder::new(&app_handle, "welcome", WindowUrl::default()).build().unwrap();
+}
+
+fn main() {
+    tauri::Builder::default()
+        .on_page_load(|window, _payload| {
+            println!("Page loaded on");
+        })
+        .on_window_event(|event| {
+            println!("Window event, {} {:?}", event.window().label() ,event.event());
+        })
+        .invoke_handler(tauri::generate_handler![spawn_window])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { invoke } from "@tauri-apps/api/tauri";
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    invoke("spawn_window");
+  });
+});
